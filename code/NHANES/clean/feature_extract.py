534d636613d4837796821912a97658a8967dc308
hvrlxy <hle5@gustavus.edu>
2022-12-06 00:57:38 -0500

    fix bugs in feature extraction code

--- code/NHANES/clean/feature_extract.py
+++ code/NHANES/clean/feature_extract.py
@@ -36,7 +36,7 @@ class ActigraphSummary:
         '''
         # rename the axis to be timestamp, x, y, z
         self.df.rename(columns={'timestamp': 'timestamp', 'Accelerometer X': 'x', 'Accelerometer Y': 'y', 'Accelerometer Z': 'z'}, inplace=True)
-        self.df = self.apply_filter_to_all_axis()
+        self.apply_filter_to_all_axis()
         # loop through the data in 10s window (80Hz * 10s = 800 samples)
         window_size = 800
         length_data = len(self.df)
@@ -72,34 +72,18 @@ class ActigraphSummary:
         # loggin the skewness of the x axis
         print("Calculating the skewness of the x axis")
         new_df['x_skew'] = x_series.groupby(np.arange(len(x_series.index)) // 800).skew()
-        # loggin the kurtosis of the x axis
-        # print("Calculating the kurtosis of the x axis")
-        # new_df['x_kurtosis'] = x_series.groupby(np.arange(len(x_series.index)) // 800).kurtosis()        
-        # loggin the fft mean of the x axis
-        print("Calculating the fft mean of the x axis")
-        new_df['x_fft_mean'] = x_series.groupby(np.arange(len(x_series.index)) // 800).apply(self.get_freq_domain_features).apply(pd.Series).mean(axis=1)
-        # loggin the fft std of the x axis
-        print("Calculating the fft std of the x axis")
-        new_df['x_fft_std'] = x_series.groupby(np.arange(len(x_series.index)) // 800).apply(self.get_freq_domain_features).apply(pd.Series).std(axis=1)
-        # loggin the fft min of the x axis
-        print("Calculating the fft min of the x axis")
-        new_df['x_fft_min'] = x_series.groupby(np.arange(len(x_series.index)) // 800).apply(self.get_freq_domain_features).apply(pd.Series).min(axis=1)
-        # loggin the fft max of the x axis
-        print("Calculating the fft max of the x axis")
-        new_df['x_fft_max'] = x_series.groupby(np.arange(len(x_series.index)) // 800).apply(self.get_freq_domain_features).apply(pd.Series).max(axis=1)
-        # loggin the fft median of the x axis
-        print("Calculating the fft median of the x axis")
-        new_df['x_fft_median'] = x_series.groupby(np.arange(len(x_series.index)) // 800).apply(self.get_freq_domain_features).apply(pd.Series).median(axis=1)
-        # loggin the fft skewness of the x axis
-        print("Calculating the fft skewness of the x axis")
-        new_df['x_fft_skew'] = x_series.groupby(np.arange(len(x_series.index)) // 800).apply(self.get_freq_domain_features).apply(pd.Series).skew(axis=1)
-        # new_df['x_fft_kurtosis'] = x_series.groupby(np.arange(len(x_series.index)) // 800).apply(self.get_freq_domain_features).apply(pd.Series).kurtosis(axis=1)
-        # generate features for y axis
+        # get the frequency domain features of the x axis
+        fft_features_x = x_series.groupby(np.arange(len(x_series.index)) // 800).apply(self.get_freq_domain_features)
+        fft_x_cols = ['x_fft_dc', 'x_fft_mean', 'x_fft_std', 'x_fft_aad', 'x_fft_min', 'x_fft_max', 'x_fft_maxmin_diff', 'x_fft_median', 
+                      'x_fft_mad', 'x_fft_IQR', 'x_fft_neg_count', 'x_fft_pos_count', 'x_fft_above_mean', 'x_fft_num_peaks', 'x_fft_skew', 
+                      'x_fft_kurtosis', 'x_fft_energy', 'x_fft_sma']
+        for i in range(len(fft_x_cols)):
+            print(f"Calculating the {fft_x_cols[i]} of the x axis")
+            new_df[fft_x_cols[i]] = fft_features_x.apply(lambda x: x[i])
+        # get the time domain features of the y axis
         y_series = self.df['y']
-        # loggin the mean of the y axis
         print("Calculating the mean of the y axis")
-        new_df['y_mean'] = y_series.groupby(np.arange(len(y_series.index)) // 800).mean()
-        # loggin the std of the y axis
+        new_df['y_mean'] = self.df['y'].groupby(np.arange(len(self.df.index)) // 800).mean()
         print("Calculating the std of the y axis")
         new_df['y_std'] = y_series.groupby(np.arange(len(y_series.index)) // 800).std()
         # loggin the min of the y axis
@@ -114,34 +98,18 @@ class ActigraphSummary:
         # loggin the skewness of the y axis
         print("Calculating the skewness of the y axis")
         new_df['y_skew'] = y_series.groupby(np.arange(len(y_series.index)) // 800).skew()
-        # loggin the kurtosis of the y axis
-        # print("Calculating the kurtosis of the y axis")
-        # new_df['y_kurtosis'] = y_series.groupby(np.arange(len(y_series.index)) // 800).kurtosis()
-        # loggin the fft mean of the y axis
-        print("Calculating the fft mean of the y axis")
-        new_df['y_fft_mean'] = y_series.groupby(np.arange(len(y_series.index)) // 800).apply(self.get_freq_domain_features).apply(pd.Series).mean(axis=1)
-        # loggin the fft std of the y axis
-        print("Calculating the fft std of the y axis")
-        new_df['y_fft_std'] = y_series.groupby(np.arange(len(y_series.index)) // 800).apply(self.get_freq_domain_features).apply(pd.Series).std(axis=1)
-        # loggin the fft min of the y axis
-        print("Calculating the fft min of the y axis")
-        new_df['y_fft_min'] = y_series.groupby(np.arange(len(y_series.index)) // 800).apply(self.get_freq_domain_features).apply(pd.Series).min(axis=1)
-        # loggin the fft max of the y axis
-        print("Calculating the fft max of the y axis")
-        new_df['y_fft_max'] = y_series.groupby(np.arange(len(y_series.index)) // 800).apply(self.get_freq_domain_features).apply(pd.Series).max(axis=1)
-        # loggin the fft median of the y axis
-        print("Calculating the fft median of the y axis")
-        new_df['y_fft_median'] = y_series.groupby(np.arange(len(y_series.index)) // 800).apply(self.get_freq_domain_features).apply(pd.Series).median(axis=1)
-        # loggin the fft skew of the y axis
-        print("Calculating the fft skew of the y axis")
-        new_df['y_fft_skew'] = y_series.groupby(np.arange(len(y_series.index)) // 800).apply(self.get_freq_domain_features).apply(pd.Series).skew(axis=1)
-        # new_df['y_fft_kurtosis'] = y_series.groupby(np.arange(len(y_series.index)) // 800).apply(self.get_freq_domain_features).apply(pd.Series).kurtosis(axis=1)
-        # generate features for z axis
+        # get the frequency domain features of the y axis
+        fft_features_x = y_series.groupby(np.arange(len(y_series.index)) // 800).apply(self.get_freq_domain_features)
+        fft_y_cols = ['y_fft_dc', 'y_fft_mean', 'y_fft_std', 'y_fft_aad', 'y_fft_min', 'y_fft_max', 'y_fft_maxmin_diff', 'y_fft_median', 
+                      'y_fft_mad', 'y_fft_IQR', 'y_fft_neg_count', 'y_fft_pos_count', 'y_fft_above_mean', 'y_fft_num_peaks', 'y_fft_skew', 
+                      'y_fft_kurtosis', 'y_fft_energy', 'y_fft_sma']
+        for i in range(len(fft_y_cols)):
+            print(f"Calculating the {fft_y_cols[i]} of the y axis")
+            new_df[fft_y_cols[i]] = fft_features_x.apply(lambda x: x[i])
+        # get the time domain features of the z axis
         z_series = self.df['z']
-        # loggin the mean of the z axis
         print("Calculating the mean of the z axis")
-        new_df['z_mean'] = z_series.groupby(np.arange(len(z_series.index)) // 800).mean()
-        # loggin the std of the z axis
+        new_df['z_mean'] = z_series.groupby(np.arange(len(self.df.index)) // 800).mean()
         print("Calculating the std of the z axis")
         new_df['z_std'] = z_series.groupby(np.arange(len(z_series.index)) // 800).std()
         # loggin the min of the z axis
@@ -156,29 +124,69 @@ class ActigraphSummary:
         # loggin the skewness of the z axis
         print("Calculating the skewness of the z axis")
         new_df['z_skew'] = z_series.groupby(np.arange(len(z_series.index)) // 800).skew()
-        # # loggin the kurtosis of the z axis
-        # print("Calculating the kurtosis of the z axis")
-        # new_df['z_kurtosis'] = z_series.groupby(np.arange(len(z_series.index)) // 800).kurtosis()
-        # loggin the fft mean of the z axis
-        print("Calculating the fft mean of the z axis")
-        new_df['z_fft_mean'] = z_series.groupby(np.arange(len(z_series.index)) // 800).apply(self.get_freq_domain_features).apply(pd.Series).mean(axis=1)
-        # loggin the fft std of the z axis
-        print("Calculating the fft std of the z axis")
-        new_df['z_fft_std'] = z_series.groupby(np.arange(len(z_series.index)) // 800).apply(self.get_freq_domain_features).apply(pd.Series).std(axis=1)
-        # loggin the fft min of the z axis
-        print("Calculating the fft min of the z axis")
-        new_df['z_fft_min'] = z_series.groupby(np.arange(len(z_series.index)) // 800).apply(self.get_freq_domain_features).apply(pd.Series).min(axis=1)
-        # loggin the fft max of the z axis
-        print("Calculating the fft max of the z axis")
-        new_df['z_fft_max'] = z_series.groupby(np.arange(len(z_series.index)) // 800).apply(self.get_freq_domain_features).apply(pd.Series).max(axis=1)
-        # loggin the fft median of the z axis
-        print("Calculating the fft median of the z axis")
-        new_df['z_fft_median'] = z_series.groupby(np.arange(len(z_series.index)) // 800).apply(self.get_freq_domain_features).apply(pd.Series).median(axis=1)
-        # loggin the fft skew of the z axis
-        print("Calculating the fft skew of the z axis")
-        new_df['z_fft_skew'] = z_series.groupby(np.arange(len(z_series.index)) // 800).apply(self.get_freq_domain_features).apply(pd.Series).skew(axis=1)
-        # new_df['z_fft_kurtosis'] = z_series.groupby(np.arange(len(z_series.index)) // 800).apply(self.get_freq_domain_features).apply(pd.Series).kurtosis(axis=1)
+        # get the frequency domain features of the z axis
+        fft_features_x = z_series.groupby(np.arange(len(z_series.index)) // 800).apply(self.get_freq_domain_features)
+        fft_z_cols = ['z_fft_dc', 'z_fft_mean', 'z_fft_std', 'z_fft_aad', 'z_fft_min', 'z_fft_max', 'z_fft_maxmin_diff', 'z_fft_median', 
+                      'z_fft_mad', 'z_fft_IQR', 'z_fft_neg_count', 'z_fft_pos_count', 'z_fft_above_mean', 'z_fft_num_peaks', 'z_fft_skew', 
+                      'z_fft_kurtosis', 'z_fft_energy', 'z_fft_sma']
+        for i in range(len(fft_z_cols)):
+            print(f"Calculating the {fft_z_cols[i]} of the z axis")
+            new_df[fft_z_cols[i]] = fft_features_x.apply(lambda x: x[i])        
+        
         return new_df
+    
+    def get_time_series_features(self, signal):
+        window_size = len(signal)
+        # mean
+        sig_mean = np.mean(signal)
+        # standard deviation
+        sig_std = np.std(signal)
+        # avg absolute difference
+        sig_aad = np.mean(np.absolute(signal - np.mean(signal)))
+        # min
+        sig_min = np.min(signal)
+        # max
+        sig_max = np.max(signal)
+        # max-min difference
+        sig_maxmin_diff = sig_max - sig_min
+        # median
+        sig_median = np.median(signal)
+        # median absolute deviation
+        sig_mad = np.median(np.absolute(signal - np.median(signal)))
+        # Inter-quartile range
+        sig_IQR = np.percentile(signal, 75) - np.percentile(signal, 25)
+        # negative count
+        sig_neg_count = np.sum(s < 0 for s in signal)
+        # positive count
+        sig_pos_count = np.sum(s > 0 for s in signal)
+        # values above mean
+        sig_above_mean = np.sum(s > sig_mean for s in signal)
+        # number of peaks
+        sig_num_peaks = len(find_peaks(signal)[0])
+        # skewness
+        sig_skew = stats.skew(signal)
+        # kurtosis
+        sig_kurtosis = stats.kurtosis(signal)
+        # energy
+        sig_energy = np.sum(s ** 2 for s in signal) / window_size
+        # signal area
+        sig_sma = np.sum(signal) / window_size
+
+        return [sig_mean, sig_std, sig_aad, sig_min, sig_max, sig_maxmin_diff, sig_median, sig_mad, sig_IQR, sig_neg_count, sig_pos_count, sig_above_mean, sig_num_peaks, sig_skew, sig_kurtosis, sig_energy, sig_sma]
+
+    
+    def get_freq_domain_features(self, signal):
+        all_fft_features = []
+        window_size = len(signal)
+        signal_fft = np.abs(np.fft.fft(signal))
+        # Signal DC component
+        sig_fft_dc = signal_fft[0]
+        # aggregations over the fft signal
+        fft_feats = self.get_time_series_features(signal_fft[1:int(window_size / 2) + 1])
+
+        all_fft_features.append(sig_fft_dc)
+        all_fft_features.extend(fft_feats)
+        return all_fft_features
 
     def moving_average_filtered(self, data, window_size = 20):
         '''
--- code/NHANES/clean/process_raw_data.py
+++ code/NHANES/clean/process_raw_data.py
@@ -5,7 +5,6 @@ import numpy as np
 import scipy.stats as stats
 from feature_extract import ActigraphSummary
 import gzip
-file_lst = ['65704.csv.gz', '69247.csv.gz', '80458.csv.gz']
 
 def convert_string_to_datetime(start_time: str, start_date: str):
     '''
@@ -52,13 +51,23 @@ def read_raw_accel_data(file_name: int):
     return df
 
 def extract_and_save_features(file_name):
-    file_name = '/Users/hale/Desktop/NEU-CLASS/FinalProjectF22/data/NHANES/raw/' + file_name
-    raw_df = read_raw_accel_data(file_name)
+    full_path = '/Users/hale/Desktop/NEU-CLASS/FinalProjectF22/data/NHANES/raw/raw_sensor_file/' + file_name
+    raw_df = read_raw_accel_data(full_path)
     act_sum = ActigraphSummary(raw_df)
     feature_df = act_sum.segment_and_add_features()
     
     # save the feature_df to a csv file
-    feature_df.to_csv("/Users/hale/Desktop/NEU-CLASS/FinalProjectF22/data/NHANES/features_extracted/" + file_name, index=False)
+    feature_df.to_csv("/Users/hale/Desktop/NEU-CLASS/FinalProjectF22/data/NHANES/features_extracted/" + file_name.replace('.gz', ''), index=False)
     
-for file in file_lst:
-    extract_and_save_features(file)
+if __name__ == "__main__":
+    # get the arguments from the command line
+    start_ind = int(sys.argv[1])
+    end_ind = int(sys.argv[2])
+    
+    # list all the file in /Users/hale/Desktop/NEU-CLASS/FinalProjectF22/data/NHANES/raw/raw_sensor_file folder
+    file_lst = os.listdir("/Users/hale/Desktop/NEU-CLASS/FinalProjectF22/data/NHANES/raw/raw_sensor_file")
+    # get the first 10 files
+    file_lst = file_lst[start_ind:end_ind]
+    for file in file_lst:
+        print(file)
+        extract_and_save_features(file)
